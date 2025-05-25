b30b8ccb5b2fb3a05b9e1a0f210b6beb92d16474
Jacob K Lewis <contact@jacoblewis.me>
2025-05-24 22:39:32 -0500

    refactor: remove unused flow import and example code from index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,3 @@ export {
   TFDocType,
 } from "./elements.js";
 export { FlowGuide } from "./flowGuide.js";
-
-import { flow } from "./htmlParser.js";
-
-const htmlContent = "<div><h1>Hello</h1> World!</div>";
-const parsed = flow(htmlContent);
-console.log(parsed.q("h1").html);
